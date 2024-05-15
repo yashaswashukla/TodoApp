5bb6192e7a9e3ac497db90cd5729530e99e2d653
Yashawa <yashaswashukla@gmail.com>
2024-05-15 17:26:32 +0530

    Added mongoose database

--- backend/index.js
+++ backend/index.js
@@ -1,25 +1,44 @@
 import express from "express";
 import { createTodo, updateTodo } from "./types.js";
+import { todo } from "./db.js";
+
 const app = express();
 
 app.use(express.json());
 
-app.post("/todo", (req, res) => {
+app.post("/todo", async (req, res) => {
   const createPayload = req.body;
   const parsedPayload = createTodo.safeParse(createPayload);
   if (!parsedPayload.success) {
     res.status(411).json({ msg: "Please send the valid input" });
     return;
   }
+  await todo.create({
+    title: createPayload.title,
+    description: createPayload.description,
+    completed: false,
+  });
+  res.json({ msg: "Todo Created!" });
 });
 
-app.get("/todos", (req, res) => {});
+app.get("/todos", async (req, res) => {
+  const todos = await todo.find({});
+  console.log(todos);
+  res.json({ todos });
+});
 
-app.put("/completed", (req, res) => {
+app.put("/completed", async (req, res) => {
   const updatePayload = req.body;
-  const parsedPayload = updatePayload.safeParse(parsedPayload);
+  const parsedPayload = updateTodo.safeParse(updatePayload);
   if (!parsedPayload.success) {
     res.status(411).json({ msg: "Please send the valid input" });
     return;
   }
+
+  const updateTodo = await todo.update(
+    { _id: updatePayload.id },
+    { completed: true }
+  );
+
+  res.json({ msg: "Todo marked as completed!" });
 });
