8f7c42bc35c65da252125bc3d5c4828dffd4628e
Yashawa <yashaswashukla@gmail.com>
2024-05-16 17:07:55 +0530

    Updated fetch

--- frontend/src/components/CreateTodo.jsx
+++ frontend/src/components/CreateTodo.jsx
@@ -1,12 +1,17 @@
-import React from "react";
+import React, { useState } from "react";
 
 function CreateTodo() {
+  const [title, setTitle] = useState("");
+  const [description, setDesc] = useState("");
   return (
     <div>
       <input
         style={{ padding: 10, margin: 10 }}
         type="text"
         placeholder="Title"
+        onChange={(e) => {
+          const value = e.target.value;
+        }}
       ></input>
       <br />
       <input
@@ -15,7 +20,19 @@ function CreateTodo() {
         placeholder="Description"
       ></input>
       <br />
-      <button>Add a Todo</button>
+      <button
+        onClick={() => {
+          fetch("http://localhost:3000/todos", {
+            method: "POST",
+            body: {
+              title: "Hello",
+              description: "Nice",
+            },
+          });
+        }}
+      >
+        Add a Todo
+      </button>
     </div>
   );
 }
